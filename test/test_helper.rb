b8ea4511b9e6c5ced1a1b48bd0439b62f681b0cc
Spencer Steffen <spencer@citrusme.com>
2011-06-29 01:16:31 -0700

    always kqueue if kqueue?

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,7 @@ require 'bundler/setup'
 require 'shoulda'
 require 'autocuke'
 
+EM.kqueue = EM.kqueue?
+
 # require support
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].map{ |f| require f }
