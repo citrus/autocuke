2d44ada3f81b8c2b4dea0ed92c851e9e901b539b
Spencer Steffen <spencer@citrusme.com>
2011-06-28 17:32:01 -0700

    adding basic options and defaults

--- lib/autocuke.rb
+++ lib/autocuke.rb
@@ -1,5 +1,15 @@
+require "fileutils"
 require "autocuke/runtime"
 require "autocuke/version"
 
 module Autocuke
+  
+  class NoFileError < Exception; end
+  
+  def self.defaults
+    {
+      :files => Dir[File.join(Dir.getwd, "features/**/*.feature")]
+    }
+  end
+
 end
--- lib/autocuke/runtime.rb
+++ lib/autocuke/runtime.rb
@@ -4,14 +4,20 @@ require "autocuke/handler"
 module Autocuke
   class Runtime
     
+    attr_reader :options
+    
     def initialize(options={})
+      @options = Autocuke.defaults.merge(options)
       puts "HELLO!"
       puts options.inspect
       super
     end
     
     def run!
+      raise Autocuke::NoFileError.new("No files given to watch!") unless files = options[:files]
+    
       puts "OMG!?!"
+      puts files.inspect
 
       # file watching requires kqueue on OSX
       EM.kqueue = true if EM.kqueue?
